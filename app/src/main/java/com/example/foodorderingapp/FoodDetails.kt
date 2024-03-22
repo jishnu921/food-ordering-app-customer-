4a58756836147c33f0c6bee60923c285d7bd6955
abjeet16 <avjeetyadav00@gmail.com>
2024-03-22 22:43:35 +0530

    still doing

--- app/src/main/java/com/example/foodorderingapp/FoodDetails.kt
+++ app/src/main/java/com/example/foodorderingapp/FoodDetails.kt
@@ -1,11 +1,18 @@
 package com.example.foodorderingapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.foodorderingapp.databinding.ActivityFoodDetailsBinding
 
 class FoodDetails : AppCompatActivity() {
+    private lateinit var FoodName:String
+    private lateinit var foodDescription:String
+    private lateinit var FoodPrice:String
+    private lateinit var foodIngredent:String
+    private lateinit var foodImage:String
     private val binding : ActivityFoodDetailsBinding by lazy {
         ActivityFoodDetailsBinding.inflate(layoutInflater)
     }
@@ -13,18 +20,24 @@ class FoodDetails : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val FoodName = intent.getStringExtra("foodName")
-        val FoodPrice = intent.getStringExtra("foodPrice")
-        val foodDescription = intent.getStringExtra("foodDescription")
-        val foodIngredent = intent.getStringExtra("foodIngredent")
+        FoodName = intent.getStringExtra("foodName").toString()
+        FoodPrice = intent.getStringExtra("foodPrice").toString()
+        foodDescription = intent.getStringExtra("foodDescription").toString()
+        foodIngredent = intent.getStringExtra("foodIngredent").toString()
+        foodImage = intent.getStringExtra("foodImage").toString()
         //setting food details
-        binding.foodname.text = FoodName
-        binding.FoodDescription.text = foodDescription
-        binding.Ingredients.text = foodIngredent
+        setFoodDetails()
 
         binding.backButtonFoodDetails.setOnClickListener(){
             finish()
         }
+    }
+
+    private fun setFoodDetails() {
+        binding.foodname.text = FoodName
+        binding.FoodDescription.text = foodDescription
+        binding.Ingredients.text = foodIngredent
+        Glide.with(this).load(Uri.parse(foodImage)).into(binding.foodImage)
     }
 
 }
