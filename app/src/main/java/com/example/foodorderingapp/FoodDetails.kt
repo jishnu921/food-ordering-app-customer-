dfc967802056d37dea166f99397a4562b8d4f1e6
abjeet16 <avjeetyadav00@gmail.com>
2024-03-23 15:32:33 +0530

    still doing

--- app/src/main/java/com/example/foodorderingapp/FoodDetails.kt
+++ app/src/main/java/com/example/foodorderingapp/FoodDetails.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.foodorderingapp.databinding.ActivityFoodDetailsBinding
+import com.example.foodorderingapp.datamodel.cartItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class FoodDetails : AppCompatActivity() {
     private lateinit var FoodName:String
@@ -13,12 +17,15 @@ class FoodDetails : AppCompatActivity() {
     private lateinit var FoodPrice:String
     private lateinit var foodIngredent:String
     private lateinit var foodImage:String
+    private lateinit var firebaseAuth:FirebaseAuth
     private val binding : ActivityFoodDetailsBinding by lazy {
         ActivityFoodDetailsBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        firebaseAuth =  FirebaseAuth.getInstance()
 
         FoodName = intent.getStringExtra("foodName").toString()
         FoodPrice = intent.getStringExtra("foodPrice").toString()
@@ -31,6 +38,28 @@ class FoodDetails : AppCompatActivity() {
         binding.backButtonFoodDetails.setOnClickListener(){
             finish()
         }
+        binding.addToCart.setOnClickListener(){
+            addItemToCart()
+        }
+    }
+
+    private fun addItemToCart() {
+        val database =  FirebaseDatabase.getInstance().reference
+        val userId =  firebaseAuth.currentUser?.uid?:""
+
+        //create a cart data model
+        val CartItem = cartItem(FoodName,FoodPrice,foodDescription,foodIngredent,foodImage,1)
+        // save crt item data in firebase
+        database.child("user").child(userId).child("cartItem").push().setValue(CartItem)
+            .addOnSuccessListener {
+            makeToast("item added")
+        }.addOnFailureListener(){
+            makeToast("item adding failed")
+        }
+    }
+
+    private fun makeToast(s: String) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show()
     }
 
     private fun setFoodDetails() {
---
+++ app/src/main/java/com/example/foodorderingapp/datamodel/cartItem.kt
@@ -0,0 +1,8 @@
+package com.example.foodorderingapp.datamodel
+
+data class cartItem(val foodname:String?=null,
+                    val foodPrice:String?=null,
+                    val foodDescription:String?=null,
+                    val foodIngradent:String?=null,
+                    val foodimage:String?=null,
+                    val foodQuanity:Int?=null)
