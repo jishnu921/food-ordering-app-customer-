8d21fc929d19586b777d91d38bb8b65422bd2064
abjeet16 <avjeetyadav00@gmail.com>
2024-03-25 16:59:42 +0530

    still doing

--- app/src/main/java/com/example/foodorderingapp/splashScreen.kt
+++ app/src/main/java/com/example/foodorderingapp/splashScreen.kt
@@ -6,17 +6,24 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
 
 class splashScreen : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         //turn off night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,startScreen::class.java));
+            if (firebaseAuth.currentUser!=null)
+                startActivity(Intent(this,MainActivity::class.java))
+            else
+                startActivity(Intent(this,startScreen::class.java));
             finish();
         },2000)
     }
