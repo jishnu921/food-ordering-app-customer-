db36640178eca8b870488d2b9aedba9426431c6e
abjeet16 <avjeetyadav00@gmail.com>
2024-03-30 23:45:07 +0530

    still doing

--- app/src/main/java/com/example/foodorderingapp/fragments/historyFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/fragments/historyFragment.kt
@@ -11,15 +11,16 @@ import com.example.foodorderingapp.adapters.historyAdapter
 import com.example.foodorderingapp.databinding.FragmentHistoryBinding
 import com.example.foodorderingapp.datamodel.orderDetails
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class historyFragment : Fragment() {
-    private lateinit var binding:FragmentHistoryBinding
-    private lateinit var adapter:historyAdapter
+    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var adapter: historyAdapter
     private lateinit var database: FirebaseDatabase
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var userId:String
-    private var listOfOrderItem:MutableList<orderDetails> = mutableListOf()
+    private lateinit var userId: String
+    private var listOfOrderItem: MutableList<orderDetails> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,22 +32,32 @@ class historyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHistoryBinding.inflate(inflater,container,false)
-
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
         retrieveBuyHistory()
         return binding.root
     }
 
     private fun retrieveBuyHistory() {
         binding.CurrentBuyCardView.visibility = View.INVISIBLE
+        userId = firebaseAuth.currentUser?.uid ?: ""
+
+        val buyItemReference : DatabaseReference = database.reference.child("Customer").child(userId).child("Buy History")
+        val sortingQuery = buyItemReference.orderByChild("currentTime")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val PopularFoodName = listOf("burger","sandwich","fry","cake")
-        val PopulearFoodPrice = listOf("Rs110","Rs60","Rs65","Rs60")
-        val PopulearFoodImage = listOf(R.drawable.burger, R.drawable.sandwich, R.drawable.frys, R.drawable.cake)
-        adapter = historyAdapter(PopularFoodName as MutableList<String>,PopulearFoodPrice as MutableList<String>,PopulearFoodImage as MutableList<Int>)
+        val PopularFoodName = listOf("burger", "sandwich", "fry", "cake")
+        val PopulearFoodPrice = listOf("Rs110", "Rs60", "Rs65", "Rs60")
+        val PopulearFoodImage =
+            listOf(R.drawable.burger, R.drawable.sandwich, R.drawable.frys, R.drawable.cake)
+        adapter = historyAdapter(
+            PopularFoodName as MutableList<String>,
+            PopulearFoodPrice as MutableList<String>,
+            PopulearFoodImage as MutableList<Int>
+        )
         binding.RecyclierHistoryFragment.layoutManager = LinearLayoutManager(requireContext())
         binding.RecyclierHistoryFragment.adapter = adapter
 
