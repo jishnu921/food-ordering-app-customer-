a83007325b976371ec89fd1a9a7f8ddfd570c468
abjeet16 <avjeetyadav00@gmail.com>
2024-03-31 14:03:40 +0530

    still doing

--- app/src/main/java/com/example/foodorderingapp/adapters/historyAdapter.kt
+++ app/src/main/java/com/example/foodorderingapp/adapters/historyAdapter.kt
@@ -1,21 +1,27 @@
 package com.example.foodorderingapp.adapters
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.foodorderingapp.databinding.HistoryRecyclearviewLayoutBinding
 
 class historyAdapter(private val HistoryItemName:MutableList<String>,
                      private  val HistoryItemPrice:MutableList<String>,
-                     private val HistoryItemImage: MutableList<Int> ):
+                     private val HistoryItemImage: MutableList<String>,
+                     private val context: Context):
     RecyclerView.Adapter<historyAdapter.HistoryViewHolder>() {
     inner class HistoryViewHolder(private val binding:HistoryRecyclearviewLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
 
         private val Image = binding.historyItemImageView
         fun bind(position: Int) {
             binding.historyItemNameTextView.text = HistoryItemName[position]
-            Image.setImageResource(HistoryItemImage[position])
             binding.historyItemPriceTextView.text = HistoryItemPrice[position]
+            val uriString = HistoryItemImage[position]
+            val uri = Uri.parse(uriString)
+            Glide.with(context).load(uri).into(binding.historyItemImageView)
         }
 
     }
